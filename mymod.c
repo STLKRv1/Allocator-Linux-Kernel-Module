26f49d82112ab3fb0265cda1f06118af66da44a9
Umut Berk Bilgiç <umutbilgicapple@gmail.com>
2018-04-23 18:54:19 +0300

    Update mymod.c
    update for correct table print

--- mymod.c
+++ mymod.c
@@ -6,6 +6,16 @@
 #include <linux/sched/signal.h>
 #include <linux/mm.h>
 #include <linux/mm_types.h>
+#include <asm/page.h>
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
 
 //Source:http://venkateshabbarapu.blogspot.com.tr/2012/09/process-segments-and-vma.html
 static int processid = 1;
@@ -14,12 +24,16 @@ static int processid = 1;
 
 static void print_mem(struct task_struct *task)
 {
+	printk("\n ---- Print mem ---- \n");
         struct mm_struct *mm;
         struct vm_area_struct *vma;
         int count = 0;
         mm = task->mm;
 
 	unsigned long rss = get_mm_rss(mm);
+
+	unsigned long argusize = (unsigned long)mm->arg_end - (unsigned long)mm->arg_start;
+	unsigned long envisize = (unsigned long)mm->env_end - (unsigned long)mm->env_start;
 	
 	struct vm_area_struct* cur = mm->mmap;
 	
@@ -32,42 +46,62 @@ static void print_mem(struct task_struct *task)
 	unsigned long data_end = cur->vm_end;
 	unsigned long datasize = data_end - data_start;
 
-	cur = cur->vm_next;
+	cur = cur->vm_next; //move over bss
 	cur = cur->vm_next;
 	unsigned long heap_start = cur->vm_start;
 	unsigned long heap_end = cur->vm_end;
 	unsigned long heapsize = heap_end - heap_start;
 
 	cur = cur->vm_next;
-	cur = cur->vm_next;
-	cur = cur->vm_next;
-	while(cur->vm_flags != VM_STACK)
-	{
-		printk("ehehehehe\n");
+	
+	unsigned long stack_end;
+
+	while(cur->vm_next != NULL)
 		cur=cur->vm_next;
-	}
-	unsigned long stack_start = cur->vm_start;
-	unsigned long argusize = (unsigned long)mm->arg_end - (unsigned long)mm->arg_start;
-	unsigned long envisize = (unsigned long)mm->env_end - (unsigned long)mm->env_start;
+
+	unsigned long stack_start = ((cur->vm_prev)->vm_prev)->vm_start; //find stack start
+	stack_end = ((cur->vm_prev)->vm_prev)->vm_end;
+
+	unsigned long stacksize = stack_end - stack_start;
+
 
 	// to be fixed and checked from proc/<pid>/status !
 	
-        printk("\nCode: 		start = 0x%lx, end = 0x%lx, size = %lu bytes"
-               "\nData: 		start = 0x%lx, end = 0x%lx, size = %lu bytes"
-		"\nHeap:		start = 0x%lx, end = 0x%lx, size = %lu bytes"
-		 "\nMain Args:	start = 0x%lx, end = 0x%lx, size = %lu bytes"
-		 "\nEnviroment var:  start = 0x%lx, end = 0x%lx, size = %lu bytes"
-                 "\nStack: 		start = 0x%lx"
-		 "\nNumber of frames used = %lu \n"
-		 "Total virtual memory used = %lu \n",
-                 code_start, code_end, codesize,
-                 data_start, data_end, datasize,
-		 heap_start, heap_end, heapsize,
-		 mm->arg_start, mm->arg_end, argusize,
-		 mm->env_start, mm->env_end, envisize,
-                 stack_start,
-	 	 rss,
-		 mm->total_vm);
+        printk("\n%sCode:\t\t%sstart = 0x%lx, %send = 0x%lx, %ssize = %lu bytes"
+               "\n%sData:\t\t%sstart = 0x%lx, %send = 0x%lx, %ssize = %lu bytes"
+		"\n%sHeap:\t\t%sstart = 0x%lx, %send = 0x%lx, %ssize = %lu bytes"
+		 "\n%sMain Args:\t%sstart = 0x%lx, %send = 0x%lx, %ssize = %lu bytes"
+		 "\n%sEnviroment var:\t%sstart = 0x%lx, %send = 0x%lx, %ssize = %lu bytes"
+                 "\n%sStack:\t\t%sstart = 0x%lx, %send = 0x%lx, %ssize = %lu bytes"
+		 "\n%sNumber of frames used = %lu "
+		 "\n%sTotal virtual memory used = %lu",
+                 KBLU, KGRN, code_start,    KRED, code_end,    KYEL, codesize,
+                 KBLU, KGRN, data_start,    KRED, data_end,    KYEL, datasize,
+		 KBLU, KGRN, heap_start,    KRED, heap_end,    KYEL, heapsize,
+		 KBLU, KGRN, mm->arg_start, KRED, mm->arg_end, KYEL, argusize,
+		 KBLU, KGRN, mm->env_start, KRED, mm->env_end, KYEL, envisize,
+                 KBLU, KGRN, stack_start,   KRED, stack_end,   KYEL, stacksize,
+	 	 KBLU, rss,
+		 KBLU, mm->total_vm);
+}
+
+static void print_toptable(struct task_struct *task)
+{
+	struct mm_struct *mm;
+        struct vm_area_struct *vma;
+        mm = task->mm;
+
+	pgd_t *pgd_ptr;
+	unsigned long addr;
+	int i;
+
+	for (i = 0; i < 512; i++)
+	{
+		addr = i * (1UL << 39);
+		pgd_ptr = pgd_offset(mm, addr);
+		unsigned long content = pgd_ptr->pgd;
+		printk("%sAddr: %lu -> %sContent: %lu", KYEL, addr, KGRN, content);
+	}
 }
 
 static int mm_exp_load(void)
@@ -80,6 +114,7 @@ static int mm_exp_load(void)
                 if ( task->pid == processid) {
                         printk("%s[pid:%d]\n", task->comm, task->pid);
                         print_mem(task);
+			print_toptable(task);
                 }
         }
 
