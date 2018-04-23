8bf42d383aae5f14b4e2925d119624415f46e4fc
Alper Şahıstan <alpersahistan@gmail.com>
2018-04-23 03:46:41 +0300

    Fixed other memory calculations working on stack

--- mymod.c
+++ mymod.c
@@ -5,8 +5,10 @@
 #include <linux/mm.h>
 #include <linux/sched/signal.h>
 #include <linux/mm.h>
+#include <linux/mm_types.h>
 
-static int pid_mem = 1;
+//Source:http://venkateshabbarapu.blogspot.com.tr/2012/09/process-segments-and-vma.html
+static int processid = 1;
 
 
 
@@ -16,39 +18,54 @@ static void print_mem(struct task_struct *task)
         struct vm_area_struct *vma;
         int count = 0;
         mm = task->mm;
-        printk("\nThis mm_struct has %d vmas.\n", mm->map_count);
 
 	unsigned long rss = get_mm_rss(mm);
+	
+	struct vm_area_struct* cur = mm->mmap;
+	
+	unsigned long code_start = cur->vm_start;
+	unsigned long code_end = cur->vm_end;
+	unsigned long codesize = code_end - code_start;
 
-        /*for (vma = mm->mmap ; vma ; vma = vma->vm_next) {
-                printk ("\nVma number %d: \n", ++count);
-                printk("  Starts at 0x%lx, Ends at 0x%lx\n",
-                          vma->vm_start, vma->vm_end);
-        }*/ // commented out since we probably dont need it.
+	cur = cur->vm_next;
+	unsigned long data_start = cur->vm_start;
+	unsigned long data_end = cur->vm_end;
+	unsigned long datasize = data_end - data_start;
 
+	cur = cur->vm_next;
+	cur = cur->vm_next;
+	unsigned long heap_start = cur->vm_start;
+	unsigned long heap_end = cur->vm_end;
+	unsigned long heapsize = heap_end - heap_start;
 
-	unsigned long codesize = mm->end_code - mm->start_code;	
-	unsigned long datasize = mm->end_data - mm->start_data;
-	unsigned long heapsize = mm->brk - mm->start_brk;
-	unsigned long argusize = mm->arg_end - mm->arg_start;
-	unsigned long envisize = mm->env_end - mm->env_start;
+	cur = cur->vm_next;
+	cur = cur->vm_next;
+	cur = cur->vm_next;
+	while(cur->vm_flags != VM_STACK)
+	{
+		printk("ehehehehe\n");
+		cur=cur->vm_next;
+	}
+	unsigned long stack_start = cur->vm_start;
+	unsigned long argusize = (unsigned long)mm->arg_end - (unsigned long)mm->arg_start;
+	unsigned long envisize = (unsigned long)mm->env_end - (unsigned long)mm->env_start;
 
 	// to be fixed and checked from proc/<pid>/status !
 	
-        printk("\nCode  Segment start = 0x%lx, end = 0x%lx, size = %lu \n"
-                 "Data  Segment start = 0x%lx, end = 0x%lx, size = %lu \n"
-		 "Heap  Segment start = 0x%lx, end = 0x%lx, size = %lu \n"
-		 "Arg.  Segment start = 0x%lx, end = 0x%lx, size = %lu \n"
-		 "Envi  Segment start = 0x%lx, end = 0x%lx, size = %lu \n"
-                 "Stack Segment start = 0x%lx \n"
-		 "RSS = %lu \n"
-		 "Total vir. mem. = %lu \n",
-                 mm->start_code, mm->end_code, codesize,
-                 mm->start_data, mm->end_data, datasize,
-		 mm->start_brk, mm->brk, heapsize,
+        printk("\nCode: 		start = 0x%lx, end = 0x%lx, size = %lu bytes"
+               "\nData: 		start = 0x%lx, end = 0x%lx, size = %lu bytes"
+		"\nHeap:		start = 0x%lx, end = 0x%lx, size = %lu bytes"
+		 "\nMain Args:	start = 0x%lx, end = 0x%lx, size = %lu bytes"
+		 "\nEnviroment var:  start = 0x%lx, end = 0x%lx, size = %lu bytes"
+                 "\nStack: 		start = 0x%lx"
+		 "\nNumber of frames used = %lu \n"
+		 "Total virtual memory used = %lu \n",
+                 code_start, code_end, codesize,
+                 data_start, data_end, datasize,
+		 heap_start, heap_end, heapsize,
 		 mm->arg_start, mm->arg_end, argusize,
 		 mm->env_start, mm->env_end, envisize,
-                 mm->start_stack,
+                 stack_start,
 	 	 rss,
 		 mm->total_vm);
 }
@@ -57,11 +74,11 @@ static int mm_exp_load(void)
 {
         struct task_struct *task;
 
-        printk("\nGot the process id to look up as %d.\n", pid_mem);
+        //printk("\nGot the process id to look up as %d.\n", processid);
 
         for_each_process(task){
-                if ( task->pid == pid_mem) {
-                        printk("%s[%d]\n", task->comm, task->pid);
+                if ( task->pid == processid) {
+                        printk("%s[pid:%d]\n", task->comm, task->pid);
                         print_mem(task);
                 }
         }
@@ -76,7 +93,7 @@ static void mm_exp_unload(void)
 
 module_init(mm_exp_load);
 module_exit(mm_exp_unload);
-module_param(pid_mem, int, 0);
+module_param(processid, int, 0);
 
 MODULE_AUTHOR ("Dank Memes, 420@lol.com");
 MODULE_DESCRIPTION ("Print segment information");
